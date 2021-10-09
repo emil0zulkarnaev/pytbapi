d9e5c7b92f9be098192a7f22bf516f824613ce27
emil zulkarnaev <emil0zulkarnaev@gmail.com>
2021-10-10 04:47:03 +0500

    Базовые функции (отправка и изменение сообщения) готовы

--- pytbapi/Bot.py
+++ pytbapi/Bot.py
@@ -2,9 +2,11 @@
 
 import aiohttp
 import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, is_dataclass as dis_dataclass, asdict as dasdict
 from enum import Enum, unique
 from threading import Lock as tLock, Thread as tThread
+from requests import post as rpost
+from json import dumps as jdumps
 
 METHODS_URL = "https://api.telegram.org/bot"
 UPDATE_ID = 0
@@ -34,6 +36,42 @@ class Message:
     data: str
     callback_query_id: str
 
+@dataclass
+class InlineKeyboardButton:
+    text: str
+    url: object = None  # str
+    login_url: object = None    # str
+    callback_data: object = None    # str
+    switch_inline_query: object = None    # str
+    switch_inline_query_current_chat: object = None    # str
+    pay: object = None    # bool
+
+@dataclass
+class InlineKeyboardMarkup:
+    inline_keyboard: list
+
+def DataclassJSONEncoder(obj):
+    if dis_dataclass(obj):
+        obj = dasdict(obj)
+    if type(obj) == dict:
+        clear = {}
+        for k,v in obj.items():
+            if type(v) == dict or type(v) == list:
+                nested = DataclassJSONEncoder(v)
+                clear[k] = nested
+            elif v is not None:
+                clear[k] = v
+        return clear
+    elif type(obj) == list:
+        items = []
+        for item in obj:
+            if type(item) in [list, dict]:
+                nested = DataclassJSONEncoder(item)
+                items.append(nested)
+            elif item is not None:
+                items.append(item)
+        return items
+
 def CALLBACK(func):
     def worker_out(message, worker):
         func(message, worker)
@@ -42,6 +80,28 @@ def CALLBACK(func):
         worker.mu.release()
     return worker_out
 
+def Exec(request_type:str, message:Message, text:str, params) -> int:
+    URL = METHODS_URL + request_type
+
+    data = None
+    if request_type == "sendMessage":
+        if text == "": return 1
+        data = { "chat_id": message.chat_id, "text": text}
+        if "reply_markup" in params:
+            data["reply_markup"] = jdumps(DataclassJSONEncoder(params["reply_markup"]))
+    elif request_type == "editMessageText":
+        # повторяется, т.к. в дальнейшем будут добавлены методы, в которых
+        # это поле может быть пустым
+        if text == "": return 1
+        data = {"chat_id": message.chat_id, "message_id": message.message_id, "text": text}
+        if "reply_markup" in params:
+            data["reply_markup"] = jdumps(DataclassJSONEncoder(params["reply_markup"]))
+
+    resp = rpost(URL, data=data)
+    if not resp.ok:
+        print(resp.text)
+        return 3
+    return 0
 
 async def Controller(workers, queue, stopMessage, callback):
     global UPDATE_ID, STOP
@@ -53,7 +113,7 @@ async def Controller(workers, queue, stopMessage, callback):
         type_m = MessageType.M_TEXT
         for message in resp_data:
             await asyncio.sleep(0.01)
-            if worker.current_count > 0: continue
+            while worker.current_count == worker.count: await asyncio.sleep(0.01)
             type_m = MessageType.M_TEXT
             data = ""
             callback_query_id = ""
@@ -84,7 +144,6 @@ async def Controller(workers, queue, stopMessage, callback):
                 STOP = True
                 break
 
-            while worker.current_count == worker.count: await asyncio.sleep(0.01)
             worker.current_count += 1
             tThread(target=callback, args=(Message(
                 from_["first_name"],
@@ -136,11 +195,29 @@ def Listener(token:str, callback, workers=1, stopMessage="exit", delay=0.5):
 
 @CALLBACK
 def testCallback(message, worker):
-    print("here!", worker)
+    buttons = []
+    s = ""
+    if message.type_m == MessageType.M_TEXT:
+        buttons = [
+                [InlineKeyboardButton("1", callback_data="s"),InlineKeyboardButton("2", callback_data="s"),InlineKeyboardButton("3", callback_data="s")],
+                [InlineKeyboardButton("4", callback_data="s")],
+                [InlineKeyboardButton("5", callback_data="s"),InlineKeyboardButton("6", callback_data="s"),InlineKeyboardButton("7", callback_data="s")],
+                ]
+        s = "sendMessage"
+    elif message.type_m == MessageType.M_CALLBACK:
+        buttons = [
+                [InlineKeyboardButton("1", callback_data="s"),InlineKeyboardButton("2", callback_data="s"),InlineKeyboardButton("3", callback_data="s")],
+                [InlineKeyboardButton("100", callback_data="s")],
+                [InlineKeyboardButton("5", callback_data="s"),InlineKeyboardButton("6", callback_data="s"),InlineKeyboardButton("7", callback_data="s")],
+                ]
+        s = "editMessageText"
+    reply_markup = InlineKeyboardMarkup(buttons)
+    result = Exec(s, message, "Тестовое сообщение", {"reply_markup": reply_markup})
+    print("exec result", result)
 
 def test():
     token = ""
-    Listener(token, testCallback, 4, "quit")
+    Listener(token, testCallback, 3, "quit")
 
 if __name__ == "__main__":
     test()
