471d4ad763fb551f0e8646798a2eb2778a96600c
emil zulkarnaev <emil0zulkarnaev@gmail.com>
2021-10-09 23:08:33 +0500

    first

---
+++ pytbapi/Bot.py
@@ -0,0 +1,5 @@
+#-*- coding:utf-8 -*-
+
+class Bot:
+    def __init__(self):
+        print("hello, it's new bot")
---
+++ setup.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+	name="pytbapi",
+	version="0.0.1",
+	description="Simple functions to work with telegram api",
+	author="Emil Zulkarnaev",
+	author_email="emil0zulkarnaev@gmail.com",
+	packages=["pytbapi"],
+	license="LICENSE",
+	)
